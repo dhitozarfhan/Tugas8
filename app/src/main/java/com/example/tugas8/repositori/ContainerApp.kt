e6fcb3f4f0bacbe39a78b39174ef715edbd1061b
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-27 16:24:16 +0700

    menambahkan isi file ContainerApp.kt

--- app/src/main/java/com/example/tugas8/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/tugas8/repositori/ContainerApp.kt
@@ -1,2 +1,18 @@
 package com.example.tugas8.repositori
 
+import android.content.Context
+import androidx.compose.foundation.text.contextmenu.data.TextContextMenuData
+import com.example.tugas8.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
