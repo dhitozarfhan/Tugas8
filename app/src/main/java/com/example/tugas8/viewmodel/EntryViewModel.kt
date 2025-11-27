d407609f468ae64c79b432111115d55864c91b04
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-27 17:00:14 +0700

    menambahkan isi file EntryViewModel.kt

--- app/src/main/java/com/example/tugas8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/tugas8/viewmodel/EntryViewModel.kt
@@ -1,2 +1,39 @@
 package com.example.tugas8.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.tugas8.repositori.RepositoriSiswa
+import com.example.tugas8.room.Siswa
+
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
+        private set
+
+
+}
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+)
